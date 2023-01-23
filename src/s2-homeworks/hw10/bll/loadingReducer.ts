544f7b25809784a1e1ba9b3eb2e802b810c59066
germashukanna <germashuk@gmail.com>
2023-01-23 20:53:17 +0300

    HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,12 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type initStateType = typeof initState
+
+export const loadingReducer = (state: initStateType = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
 
         default:
             return state
@@ -20,3 +23,5 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+
