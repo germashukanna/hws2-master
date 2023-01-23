e02d7ae2de3e38c123dfc36ca1eca7d78c0b55d7
germashukanna <germashuk@gmail.com>
2023-01-23 22:37:32 +0300

    Fix HW11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,8 +4,10 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ color: 'secondary', size: "medium"
-                
+            sx={{
+                width: '200px',
+                color: '#00CC22',
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
